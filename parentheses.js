02d813fa6329e298cc81d7381977d88358d6e663
foysalthecode <ahmedmdfoysal123@gmail.com>
2025-11-08 13:07:17 +0600

    check parentheses through stack

---
+++ parentheses.js
@@ -0,0 +1,29 @@
+import Stack from "./stack.js";
+
+const bracketCheker = (str) => {
+  const stack = new Stack();
+  const bracketMap = {
+    ")": "(",
+    "}": "{",
+    "]": "[",
+  };
+  for (let i = 0; i < str.length; i++) {
+    const char = str[i];
+    if (char === "(" || char === "{" || char === "[") {
+      stack.push(char);
+    } else if (char === ")" || char === "}" || char === "]") {
+      if (stack.isEmpty()) {
+        return false;
+      }
+      const expected = bracketMap[char];
+      const got = stack.pop();
+      console.log("Expected :", expected, "Got :", got);
+      if (got != expected) {
+        return false;
+      }
+    }
+  }
+  return true;
+};
+
+console.log(bracketCheker("([{}])"));
